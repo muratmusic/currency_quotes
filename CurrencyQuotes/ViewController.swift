afef5b0e566bbb26d8ae83a939c1dad4f3c662ce
Murat Dzhusupov <murat.dzhusupov@desprice.com>
2020-11-01 13:29:59 +0700

    Initial commit

---
+++ CurrencyQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyQuotes
+//
+//  Created by Murat Dzhusupov on 01.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
